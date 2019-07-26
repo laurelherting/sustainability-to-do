855dca54c362a9a8bb3a5c5825cfd2f4a76486a6
Laurel Herting <laurel.herting@gmail.com>
2019-07-26 13:55:14 -0600

    edit jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>sustainability to do list</h1>, document.getElementById('root'));
