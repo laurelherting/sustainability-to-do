892c167519a78380ad0fa197f6332aa9b891500c
Laurel Herting <laurel.herting@gmail.com>
2019-07-29 15:52:57 -0600

    remove info into new file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Laurel Herting</h1>
-      <p>This is a paragraph about me...</p>
-      <ul>
-        <li>Reduce</li>
-        <li>Reuse</li>
-        <li>Repurpose</li>
-        <li>Recycle</li>
-      </ul>
-    </div>
-  )
-};
+import MyInfo from "./MyInfo.js";
 
 ReactDOM.render(
   <MyInfo />,
