0d023ccd23ee0ecd24c6af598b0911bb17a500f4
Laurel Herting <laurel.herting@gmail.com>
2019-07-29 15:53:16 -0600

    new file for info

---
+++ src/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Laurel Herting</h1>
+      <p>This is a paragraph about me...</p>
+      <ul>
+        <li>Reduce</li>
+        <li>Reuse</li>
+        <li>Repurpose</li>
+        <li>Recycle</li>
+      </ul>
+    </div>
+  )
+};
+
+export default MyInfo;
