783df6699e47361edf97f012b06fb094130ce48a
Laurel Herting <laurel.herting@gmail.com>
2019-07-31 10:37:49 -0600

    add MainContent

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import Footer from './Footer';
 function App() {
   return (
     <div>
-      <nav>
-        <h1>Hello again</h1>
-        <ul>
-          <l1>item 1</l1>
-          <l1>item 2</l1>
-          <l1>item 3</l1>
-        </ul>
-      </nav>
-      <main>
-        <p>This is where most of my content will go</p>
-      </main>
+      <Header />
+      <MainContent />
       <Footer />
     </div>
-  );
-}
+  )
+};
 
 export default App;
