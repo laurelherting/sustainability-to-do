6d0c7c6ff3416040d20a4f138d3e67d6503c356f
Laurel Herting <laurel.herting@gmail.com>
2019-08-01 15:23:24 -0600

    update for app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MyInfo from "./components/MyInfo.js";
+import App from "./App.js";
 
 ReactDOM.render(
-  <MyInfo />,
+  <App />,
   document.getElementById("root")
 );
