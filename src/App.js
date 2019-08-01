c273c02a7f2b5a45bf0cf967cf7120ec4f5a7b51
Laurel Herting <laurel.herting@gmail.com>
2019-08-01 15:22:46 -0600

    add placeholder text, checkbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Footer from './Footer';
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
     </div>
   )
 };
