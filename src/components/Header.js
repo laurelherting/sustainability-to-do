e8bcb7cbf55b558abf677447996a1ce4fc2686c2
Laurel Herting <laurel.herting@gmail.com>
2019-08-02 10:29:37 -0600

    add jsx-className for navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
-      <h3>This is my header element</h3>
-    </header>
+    <header className="navbar">This is my header</header>
   )
 };
 
