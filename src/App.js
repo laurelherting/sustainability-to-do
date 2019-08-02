7082a9306e9d0726502f48e0d27f489e28abfeb4
Laurel Herting <laurel.herting@gmail.com>
2019-08-02 10:26:08 -0600

    add components back in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Header from './components/Footer';
+
 function App() {
   return (
     <div>
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   )
 };
