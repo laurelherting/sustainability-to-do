d5bba9d0e328effcad71660613d522c0b5833430
Laurel Herting <laurel.herting@gmail.com>
2019-08-02 10:34:50 -0600

    fix typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import Header from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
