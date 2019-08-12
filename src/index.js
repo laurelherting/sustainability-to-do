fde02d72aefb4866799432bf4b0d585a007ac246
Laurel Herting <laurel.herting@gmail.com>
2019-08-12 16:46:13 -0600

    implement time of day

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 
 function App() {
   const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
 
-  return (
-    <h1>It is currently about {date.getHours() % 12} o'clock!</h1>;
-  )
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
+  return <h1>Good {timeOfDay}!</h1>;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
