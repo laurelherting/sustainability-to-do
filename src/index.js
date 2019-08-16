6fa67e76eb2ce47881fc18e454c81491dfe1995d
Laurel Herting <laurel.herting@gmail.com>
2019-08-16 12:23:30 -0600

    attach time with font color-style

--- src/index.js
+++ src/index.js
@@ -5,26 +5,22 @@ function App() {
   const date = new Date();
   const hours = date.getHours();
   let timeOfDay;
+  const styles = {
+    fontSize: 30
+  };
 
   if (hours < 12) {
     timeOfDay = "morning";
+    styles.color = "#FFFF00";
   } else if (hours >= 12 && hours < 17) {
     timeOfDay = "afternoon";
+    styles.color = "#00CED1";
   } else {
     timeOfDay = "night";
+    styles.color = "#FF1493";
   }
 
-  const styles = {
-    color: "#00FFFF",
-    backgroundColor: "#FFF",
-    fontSize: "24px"
-  };
-
-  return (
-    <h1 style={{ color: "#00FFFF", backgroundColor: "#FFF" }}>
-      Good {timeOfDay}!
-    </h1>
-  );
+  return <h1 style={styles}>Good {timeOfDay}!</h1>;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
