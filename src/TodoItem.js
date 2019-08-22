ca65967661c071d21d12884afcdeb83fbb706b98
Laurel Herting <laurel.herting@gmail.com>
2019-08-22 08:26:29 -0600

    add file for to-do list

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>text here</p>
+    </div>
+  );
+}
+
+export default todoItem;
