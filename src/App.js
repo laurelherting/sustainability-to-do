f79d5a6fa70032902fc7ec61a189704fc6e27e5a
Laurel Herting <laurel.herting@gmail.com>
2019-09-19 14:08:13 -0600

    edit properties of component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
-import TodoItem from "./TodoItem";
+import ContactCard from "./ContactCard";
 
 function App() {
   return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+    <div className="contacts">
+      <ContactCard
+        name="Bike"
+        imgURL="https://unsplash.com/photos/kJZSIXoXrUE"
+        credits="Pop & Zebra"
+      />
+      <ContactCard
+        name="Reusable Bag"
+        imgURL="https://unsplash.com/photos/ILR5ikYmXDE"
+        credits="Sylvie Tittel"
+      />
+      <ContactCard
+        name="Forest"
+        imgURL="https://unsplash.com/photos/wCLGDSpaV6g"
+        credits="Johan Anblick"
+      />
+      <ContactCard
+        name="Solar Panels"
+        imgURL="https://unsplash.com/photos/WvusC5M-TM8"
+        credits="Chelsea"
+      />
     </div>
   );
 }
