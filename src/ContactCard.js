308af46ca2f288c412db63df63907752682df3e9
Laurel Herting <laurel.herting@gmail.com>
2019-09-19 13:38:08 -0600

    add photo link, with credits

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function ContactCard() {
-  return();
+  return (
+    <div className="contact-card">
+      <img src="https://unsplash.com/photos/A59RzUpx9jA" />
+      <h3>Llama</h3>
+      <p>credits: photo by Alex Azabache</p>
+    </div>
+  );
 }
 
 export default ContactCard;
