f61fa57197d10f864d6569c6ada58f623eb6ae88
Laurel Herting <laurel.herting@gmail.com>
2019-09-19 14:05:46 -0600

    add details to components

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -3,9 +3,26 @@ import React from "react";
 function ContactCard() {
   return (
     <div className="contact-card">
-      <img src="https://unsplash.com/photos/A59RzUpx9jA" />
-      <h3>Llama</h3>
-      <p>credits: photo by Alex Azabache</p>
+      <ContactCard
+        <img src="https://unsplash.com/photos/kJZSIXoXrUE" />
+        <h3>Bike</h3>
+        <p>credits=photo by:Pop & Zebra</p>
+      />
+      <ContactCard
+        <img src="https://unsplash.com/photos/ILR5ikYmXDE" />
+        <h3>Reusable Bag</h3>
+        <p>credits=photo by:Sylvie Tittel</p>
+      />
+      <ContactCard
+        <img src="https://unsplash.com/photos/wCLGDSpaV6g" />
+        <h3>Forest</h3>
+        <p>credits=photo by:Johan Anblick</p>
+      />
+      <ContactCard
+        <img src="https://unsplash.com/photos/WvusC5M-TM8" />
+        <h3>Solar Panels</h3>
+        <p>credits=photo by:Chelsea</p>
+      />
     </div>
   );
 }
