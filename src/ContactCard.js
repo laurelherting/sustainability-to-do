1bbea8329241138c2a7f73cd1d85ed572c617f74
Laurel Herting <laurel.herting@gmail.com>
2019-09-25 16:09:28 -0600

    add props

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,28 +1,12 @@
 import React from "react";
 
-function ContactCard() {
+function ContactCard(props) {
+  // console.log(props);
   return (
     <div className="contact-card">
-      <ContactCard
-        <img src="https://unsplash.com/photos/kJZSIXoXrUE" />
-        <h3>Bike</h3>
-        <p>credits=photo by:Pop & Zebra</p>
-      />
-      <ContactCard
-        <img src="https://unsplash.com/photos/ILR5ikYmXDE" />
-        <h3>Reusable Bag</h3>
-        <p>credits=photo by:Sylvie Tittel</p>
-      />
-      <ContactCard
-        <img src="https://unsplash.com/photos/wCLGDSpaV6g" />
-        <h3>Forest</h3>
-        <p>credits=photo by:Johan Anblick</p>
-      />
-      <ContactCard
-        <img src="https://unsplash.com/photos/WvusC5M-TM8" />
-        <h3>Solar Panels</h3>
-        <p>credits=photo by:Chelsea</p>
-      />
+        <img src={props.imgUrl}/>
+        <h3>{props.name}</h3>
+        <p>credits: {props.credits}</p>
     </div>
   );
 }
