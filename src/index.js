5e47834c7dd9e1a0fbaf4d4c459873410a5cfaf3
Laurel Herting <laurel.herting@gmail.com>
2019-09-26 14:35:52 -0600

    adjust file for reusable components

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ContactCard from "./ContactCard";
 
 function App() {
-  const date = new Date();
-  // const date = new Date(2019, 8, 16, 15); -- test
-  const hours = date.getHours();
-  let timeOfDay;
-  const styles = {
-    fontSize: 30
-  };
-
-  if (hours < 12) {
-    timeOfDay = "morning";
-    styles.color = "#FFFF00";
-  } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon";
-    styles.color = "#00CED1";
-  } else {
-    timeOfDay = "night";
-    styles.color = "#FF1493";
-  }
-
-  return <h1 style={styles}>Good {timeOfDay}!</h1>;
+  return <div>text here</div>;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
