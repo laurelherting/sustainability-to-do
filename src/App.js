31be0d6fc03f3faf2bd19a0225eee7b265f7bef3
Laurel Herting <laurel.herting@gmail.com>
2019-09-30 10:32:12 -0600

    wip-add Plan component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from "react";
-import ContactCard from "./ContactCard";
+
+import Plan from "./Plan";
 
 function App() {
   return (
-    <div className="contacts">
-      <ContactCard
-        name="Bike"
-        imgUrl="https://unsplash.com/photos/kJZSIXoXrUE"
-        credits="Pop & Zebra"
-      />
-      <ContactCard
-        name="Reusable Bag"
-        imgUrl="https://unsplash.com/photos/ILR5ikYmXDE"
-        credits="Sylvie Tittel"
-      />
-      <ContactCard
-        name="Forest"
-        imgUrl="https://unsplash.com/photos/wCLGDSpaV6g"
-        credits="Johan Anblick"
-      />
-      <ContactCard
-        name="Solar Panels"
-        imgUrl="https://unsplash.com/photos/WvusC5M-TM8"
-        credits="Chelsea"
-      />
+    <div>
+      <Plan />
     </div>
   );
 }
