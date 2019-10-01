eebcb926b027ff8877e2646df18a52442f2258e1
Laurel Herting <laurel.herting@gmail.com>
2019-10-01 12:32:27 -0600

    add block styling, with break line

--- src/Plan.js
+++ src/Plan.js
@@ -3,8 +3,11 @@ import React from "react";
 function ClimateChangePlan(props) {
   return (
     <div>
-      <h3>Solution: {props.solution}</h3>
+      <h3 style={{ display: props.solution ? "block" : "none" }}>
+        Solution: {props.solution}
+      </h3>
       <h3>Details: {props.details}</h3>
+      <br />
     </div>
   );
 }
