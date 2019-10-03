11cadef590d089368ee2ae492e1c77ac926e6c0d
Laurel Herting <laurel.herting@gmail.com>
2019-10-03 12:01:08 -0600

    update styling, create resusable component

--- src/Plan.js
+++ src/Plan.js
@@ -6,7 +6,9 @@ function ClimateChangePlan(props) {
       <h3 style={{ display: !props.solution && "none" }}>
         Solution: {props.solution}
       </h3>
-      <h3>Details: {props.details}</h3>
+      <h3 style={{ color: !props.details && "#303030" }}>
+        Details: {props.details}
+      </h3>
       <hr />
     </div>
   );
