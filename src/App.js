ece9288a596b510978b41eae413b1cca6426d289
Laurel Herting <laurel.herting@gmail.com>
2019-10-10 14:42:43 -0600

    update for new map

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from "react";
 
-import Plan from "./Plan";
-import planData from "./planData";
-
 function App() {
-  const planComponents = planData.map(plan =>
-    <plan key={plan.id} solution={plan.solution} details={plan.details} />)
-
-  return (
-    <div>
-      /* Plan information adapated from the following website:
-      https://www.denvergov.org/content/denvergov/en/mayors-office/
-      newsroom/2019/mayor--council-announce-new-plan-to-address-climate.html */
-      {planComponents}
-    </div>
-  );
+  return <div>information</div>;
 }
 
 export default App;
