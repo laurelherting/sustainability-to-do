95b6960e1dbf1a6e8e41f4fcbe3391bc6b9827c4
Laurel Herting <laurel.herting@gmail.com>
2019-10-10 11:21:29 -0600

    wip-array variable

--- src/planData.js
+++ src/planData.js
@@ -1,7 +1,27 @@
 const planData = [
   {
     id: 1,
-    keyPoint: "Information here."
+    details: "Cars and Global Warming."
+  },
+  {
+    id: 2,
+    solution: "Establishing a new Office of Climate Action, Sustainability and Resiliency
+    that will report directly to the Mayor.",
+    details: "The office will be established by ordinance with City Council approval
+    by Oct. 31 and will be fully functional by July 1, 2020. It will combine personnel from
+    the existing Office of Sustainability and the Denver Department of Public Health &
+    Environment. The Mayor also intends to propose an additional $8 million next year to
+    expand efforts to reduce greenhouse gas emissions."
+  },
+  {
+    id: 3,
+    solution: "Immediately launching a formal process to examine Denver’s
+    current climate work, identify gaps, analyze funding needs and engage
+    experts, interested stakeholders and a wide range of community members.",
+    details: "The process will be professionally facilitated and led by both
+    the Administration and members of Council. It will conclude by the end of
+    May 2020, giving Council ample time to consider referring a possible
+    measure to the November 2020 ballot."
   }
 ]
 export default planData;
