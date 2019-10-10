7cb390a543c35a9da791f8debd9fba1f3fd83a53
Laurel Herting <laurel.herting@gmail.com>
2019-10-10 15:55:22 -0600

    wip-add id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Product from "./Product";
+import productsData from "./climateChangeProducts";
 
 function App() {
-  return <div>information</div>;
+  const productComponents = productsData.map(item => <Product product={item} />);
+  return (
+  <div>{ productComponents }</div>
+  )
 }
 
 export default App;
