62ba9765ec56883f02505a6f530b631b1b80d695
Laurel Herting <laurel.herting@gmail.com>
2019-11-22 16:04:00 -0700

    wip-create new class for comparison

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>text goes here</div>
-  );
+// function App() {
+  // return (
+    // <div>text goes here</div>
+  // );
+// }
+
+class App extends React.Component {
+  dateMethodHere() {
+
+  }
+  render() {
+     const style = this.dateMethodHere();
+    return (
+      <div><h1>Text goes here</h1></div>
+    )
+  }
 }
 
 export default App;
