70806e5c3feb18a242ca8a1626f2656713831854
Laurel Herting <laurel.herting@gmail.com>
2019-11-25 13:32:46 -0700

    wip-add constructor for state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
   render() {
     return (
       <div><h1>state</h1></div>
-    )
+    );
   }
 }
 
