b1e7306f20dec3c3fb3120af32ef744471faa880
Laurel Herting <laurel.herting@gmail.com>
2019-11-25 13:21:27 -0700

    wip-state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
 
-// function App(props) {
-  // return (
-    // <div>
-      // <h1>{props.text</h1>
-    // </div>
-  // );
-// }
-
 class App extends React.Component {
-  dateMethodHere() {
-
-  }
   render() {
     return (
-      <div><h1>this.props.text</h1></div>
+      <div><h1>state</h1></div>
     )
   }
 }
