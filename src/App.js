5ec6f9fb4019c146098d4609d079a0b17e03231c
Laurel Herting <laurel.herting@gmail.com>
2019-11-25 09:39:00 -0700

    edit props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-// function App() {
+// function App(props) {
   // return (
-    // <div>text goes here</div>
+    // <div>
+      // <h1>{props.text</h1>
+    // </div>
   // );
 // }
 
@@ -12,7 +14,7 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div><h1>Text goes here</h1></div>
+      <div><h1>this.props.text</h1></div>
     )
   }
 }
