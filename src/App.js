e6b4d82a6b6223f4235e2446bdea750d246b1cd1
Laurel Herting <laurel.herting@gmail.com>
2019-12-02 12:33:53 -0700

    wip-edit state

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Is state important to know? {this.state.answer}</h1>
-        <ChildComponent answer={this.state.answer}/>
+        <h1>{this.state.name}</h1>
+        <h3>{this.state.number}</h3>
       </div>
     );
   }
