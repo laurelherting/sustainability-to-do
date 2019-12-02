143aafb7c808fb725a5e8986c7c5bbaf8ca342c1
Laurel Herting <laurel.herting@gmail.com>
2019-12-02 15:56:15 -0700

    update component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {Component} from "react";
 
-class App extends React.Component {
+class App extends Component{
   constructor() {
     super();
     this.state = {
-      answer: "Yes"
+      name: "Jim",
+      number: "10"
     };
   }
   render() {
