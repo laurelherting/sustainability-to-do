e08be16a8d780d81bf6b5086e7fc5d5c9d43118e
Laurel Herting <laurel.herting@gmail.com>
2019-12-03 09:42:59 -0700

    change function prep for handler

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import React, {Component} from "react";
+import React from "react";
 
-class App extends Component{
-  constructor() {
-    super();
-    this.state = {
-      name: "Jim",
-      number: "10"
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>{this.state.name}</h1>
-        <h3>{this.state.number} sustainability changes this year</h3>
-      </div>
-    );
-  }
+function App() {
+    return <div>put onClick handler here</div>; 
 }
 
 export default App;
