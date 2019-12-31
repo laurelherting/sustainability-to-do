620c0179f252a4e6648f52fb40ac8684b68e8faf
Laurel Herting <laurel.herting@gmail.com>
2019-12-31 13:33:15 -0700

    wip-new file, remove semi-colons for consistency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from "react"
 
 function App() {
     return (
       <div>
         <button onClck={() => console.log("I was clicked!")}>Click here</button>
       </div>
-    );
+    )
 }
 
 export default App;
