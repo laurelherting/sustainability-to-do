353ab8e1f7df0ff299173b4f8539c20227887e74
Laurel Herting <laurel.herting@gmail.com>
2019-12-31 13:34:19 -0700

    update props

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-function TodoItem() {
+function TodoItem(props) {
   return (
-    <div></div>
+    <div className="todo-item">
+      <input type="checkbox" checked={props.item.completed} />
+      <p>{props.item.text</p>
+    </div>
   )
 }
 
