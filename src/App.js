77d8b6e445fc9a1efe2782211ad67a90dfe6ea81
Laurel Herting <laurel.herting@gmail.com>
2020-01-03 15:01:43 -0700

    edit function to include previous version of state

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ class App extends React.Component {
   }
 
   handleClick() {
-    this.setState({ count: 1 })
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
   }
 
   render() {
