b479fe7b477471e38f79fb158cb09231e232c04e
Laurel Herting <laurel.herting@gmail.com>
2020-01-15 16:03:29 -0700

    wip-update change

--- src/App.js
+++ src/App.js
@@ -12,20 +12,23 @@ class App extends React.Component {
   }
 
   handleChange(id) {
-    console.log("Changed", id)
-  }
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    return {
+      todos: updatedTodos
+    }
+  })
+}
 
-  render() {
-    const todoItems = this.state.todos.map(item => 
-      <TodoItem key={item.id} item={item}
-        handleChange={this.handleChange} />)
-
-    return (
-      <div className = "todo-list">
-        {todoItems}
-      </div>
-    )
-  }
+render() {
+  const todoItems = this.state.todos.map(item => 
+    <TodoItem key={item.id} item={item}
+    handleChange={this.handleChange} />)
 }
 
 export default App
