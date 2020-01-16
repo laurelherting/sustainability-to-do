8a14365b647af65f48ceee72a83523ae9cd4b4cb
Laurel Herting <laurel.herting@gmail.com>
2020-01-16 16:40:05 -0700

    wip-edit todos function

--- src/todosData.js
+++ src/todosData.js
@@ -1,12 +1,34 @@
-
-import React from "react"
-
-function todosData() {
-  return (
-    <div>
-      <p>text here</p>
-    </div>
-  )
-}
+const todosData = [
+  {
+    id: 1,
+    text: "Crochet 4 produce bags",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Bring bags to party to donate",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Scroll Instagram for water bottle resusables",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Post pics to i feed",
+    completed: false
+  },
+  {
+    id: 5,
+    text: "Write letter to one major company-request help for climate change",
+    completed: false
+  },
+  {
+    id: 6,
+    text: "Donate to one environmental cause",
+    completed: false
+  }
+]
 
 export default todosData
