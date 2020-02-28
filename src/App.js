f691689a24cb7eb3edf27e032f7a157212911a57
Laurel Herting <laurel.herting@gmail.com>
2020-02-28 16:00:27 -0700

    wip-lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React from "react"
-import TodoItem from "./TodoItem"
-import todosData from "./todosData"
+import React from "react";
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
-    super()
-    this.state = {
-      todos: todosData
-    }
-    this.handleChange = this.handleChange.bind(this)
+    super();
+    this.state = {};
   }
 
-  handleChange(id) {
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    })
-    return {
-      todos: updatedTodos
-    }
-  })
+  static getDerivedStateFromProps(props, state) {}
+
+  getSnapshotBeforeUpdate() {
+    // create backup of data/way things are
+  }
+  render() {
+    return <div>code here</div>;
+  }
 }
 
-render() {
-  const todoItems = this.state.todos.map(item => 
-    <TodoItem key={item.id} item={item}
-    handleChange={this.handleChange} />)
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
