f9364e581a79a9f3cdb7fc3fcc2ee385d470ce52
Laurel Herting <laurel.herting@gmail.com>
2020-03-05 15:22:54 -0700

    wip-component

--- src/App.js
+++ src/App.js
@@ -6,26 +6,6 @@ class App extends Component {
     this.state = {};
   }
 
-  static getDerivedStateFromProps(props, state) {
-    // return updated state based on props
-  }
-
-  getSnapshotBeforeUpdate() {
-    // create backup of data/way things are
-  }
-
-  // componentWillMount() {}
-
-  componentDidMount() {}
-
-  // componentWillReceiveProps(nextProps) {
-  // if (nextProps.whatever !== this.props.whatever)
-  // }
-
-  shouldComponentUpdate(nextProps, nextState) {}
-
-  // componentWillUpdate() {}
-
   render() {
     return <div>code here</div>;
   }
