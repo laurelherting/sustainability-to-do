222007a849f6758484788c38dce19fce5f88025c
Laurel Herting <laurel.herting@gmail.com>
2020-03-09 16:33:40 -0600

    update if else statement

--- src/Conditional.js
+++ src/Conditional.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Conditional(props) {
-  return <h1>conditional rendering stuff here</h1>;
+  if (props.isLoading === true) {
+    return <h1>Loading...</h1>;
+  } else {
+    return <h1>conditional rendering stuff here</h1>;
+  }
 }
 
 export default Conditional;
