01045e1f65df5a36370032718d7d4a849dbae317
Laurel Herting <laurel.herting@gmail.com>
2020-03-09 13:22:31 -0600

    wip-state

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   }
 
   render() {
-    return <div>Conditional isLoading={} /></div>;
+    return <div>Conditional isLoading={this.state.isLoading} /></div>;
   }
 }
 
