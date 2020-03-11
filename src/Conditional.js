0b17a9e73dba52cc1cd681d084c3fc6e3edfd80f
Laurel Herting <laurel.herting@gmail.com>
2020-03-11 15:39:49 -0600

    add details to note

--- src/Conditional.js
+++ src/Conditional.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Conditional(props) {
+  props.isLoading === true ? <h1>Loading...</h1> : <h1>Rendering</h1>;
   if (props.isLoading === true) {
     return <h1>Loading...</h1>;
   } else {
