13b74a8e80c6707a279b8f69cca6d9b3332a0d1e
Laurel Herting <laurel.herting@gmail.com>
2020-03-24 14:57:29 -0600

    edit file for form use

--- src/App.js
+++ src/App.js
@@ -3,27 +3,14 @@ import React from "react";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      isLoggedIn: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => {
-      return {
-        isLoggedIn: !prevState.isLoggedIn
-      };
-    });
+    this.state = {};
   }
 
   render() {
-    let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN";
-    let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out";
     return (
-      <div>
-        <button onClick={this.handleClick}>{buttonText}</button>
-      </div>
+      <form>
+        <input />
+      </form>
     );
   }
 }
