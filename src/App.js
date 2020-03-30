608e17ab9669a3464ae17406fae0ac453cb25966
Laurel Herting <laurel.herting@gmail.com>
2020-03-30 17:38:49 -0600

    add option values

--- src/App.js
+++ src/App.js
@@ -81,9 +81,13 @@ class App extends React.Component {
           onChange={this.handleChange}
           name="bestDay"
         >
-          <option></option>
-          <option></option>
-          <option></option>
+          <option value="Sunday">Sunday</option>
+          <option value="Monday">Monday</option>
+          <option value="Tuesday">Tuesday</option>
+          <option value="Wednesday">Wednesday</option>
+          <option value="Thursday">Thursday</option>
+          <option value="Friday">Friday</option>
+          <option value="Saturday">Saturday</option>
         </select>
 
         <h1>
