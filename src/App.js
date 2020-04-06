fbaa52470a2f8ad8b2a5474a7cb61c11d38a255e
Laurel Herting <laurel.herting@gmail.com>
2020-04-06 15:42:46 -0600

    edit existing elements properties

--- src/App.js
+++ src/App.js
@@ -26,84 +26,92 @@ class App extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="firstName"
-          value={this.state.firstName}
-          placeholder="First Name"
-          onChange={this.handleChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="lastName"
-          value={this.state.lastName}
-          placeholder="Last Name"
-          onChange={this.handleChange}
-        />
-
-        <textarea value={"default value"} onChange={this.handleChange} />
-
-        <br />
-
-        <label>
+      <main>
+        <form onSubmit={this.handleSubmit}>
           <input
-            type="checkbox"
-            name="isInterested"
-            checked={this.state.isInterested}
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            placeholder="First Name"
             onChange={this.handleChange}
-          />{" "}
-          Is interested?
-        </label>
-        <br />
-        <label>
+          />
+          <br />
           <input
-            type="radio"
-            name="gender"
-            value="male"
-            checked={this.state.gender === "male"}
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            placeholder="Last Name"
             onChange={this.handleChange}
-          />{" "}
-          Male
-        </label>
-        <br />
-        <label>
-          <input
-            type="radio"
-            name="gender"
-            value="female"
-            checked={this.state.gender === "female"}
+          />
+
+          <textarea value={"default value"} onChange={this.handleChange} />
+
+          <br />
+
+          <label>
+            <input
+              type="checkbox"
+              name="isInterested"
+              checked={this.state.isInterested}
+              onChange={this.handleChange}
+            />{" "}
+            Is interested?
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={this.state.gender === "male"}
+              onChange={this.handleChange}
+            />{" "}
+            Male
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={this.state.gender === "female"}
+              onChange={this.handleChange}
+            />{" "}
+            Female
+          </label>
+
+          {/* Create radio buttons for destination here */}
+
+          <select
+            value={this.state.bestDay}
             onChange={this.handleChange}
-          />{" "}
-          Female
-        </label>
+            name="bestDay"
+          >
+            <option value="Sunday">Sunday</option>
+            <option value="Monday">Monday</option>
+            <option value="Tuesday">Tuesday</option>
+            <option value="Wednesday">Wednesday</option>
+            <option value="Thursday">Thursday</option>
+            <option value="Friday">Friday</option>
+            <option value="Saturday">Saturday</option>
+          </select>
 
-        {/* Create radio buttons for destination here */}
+          {/* Create radio buttons for tasks here */}
 
-        <select
-          value={this.state.bestDay}
-          onChange={this.handleChange}
-          name="bestDay"
-        >
-          <option value="Sunday">Sunday</option>
-          <option value="Monday">Monday</option>
-          <option value="Tuesday">Tuesday</option>
-          <option value="Wednesday">Wednesday</option>
-          <option value="Thursday">Thursday</option>
-          <option value="Friday">Friday</option>
-          <option value="Saturday">Saturday</option>
-        </select>
-
-        {/* Create radio buttons for tasks here */}
-
-        <h1>
-          {this.state.firstName} {this.state.lastName}
-        </h1>
-        <h2>You are a {this.state.gender}</h2>
-        <h2>Your best day is {this.state.bestDay}</h2>
-        <button>Submit</button>
-      </form>
+          <button>Submit</button>
+        </form>
+        <hr />
+        <h2>Entered Information:</h2>
+        <p>
+          Your name: {this.state.firstName} {this.state.lastName}
+        </p>
+        <p>You are a {this.state.gender}</p>
+        <p>Your best day is {this.state.bestDay}</p>
+        <p>
+          Your tasks:
+          {/* tasks here, comma separated */}
+        </p>
+      </main>
     );
   }
 }
